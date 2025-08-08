c18f13e3ea886071e1a93a338a6528dbce92d2db
juujacometti <julyajacometty@gmail.com>
2025-08-08 13:41:58 -0300

    Lista de exercícios JS 01 - Pendente

--- ListaExercícios/Lista01/Lista01.js
+++ ListaExercícios/Lista01/Lista01.js
@@ -209,6 +209,22 @@
 // A jornada de trabalho semanal de um funcionário é de 40 horas. O funcionário que trabalhar mais de 40 horas receberá hora extra, cujo cálculo é o valor da hora regular com um acréscimo de 50%. 
 // Escreva um algoritmo que leia o número de horas trabalhadas em um mês, o salário por hora e escreva o salário total do funcionário, que deverá ser acrescido das horas extras, caso tenham sido trabalhadas (considere que o mês possua 4 semanas exatas).
 
+alert("Verificação de recebimento de hora extra.");
+
+let horasTrabalhadasMes = prompt("Informe quantas horas você trabalhou durante esse mês: ");
+let salarioHora = prompt("Informe quanto é o seu salário por hora: ");
+let salarioTotal;
+
+let horaExtra = horasTrabalhadasMes - (40 * 4);
+
+if (horaExtra <= 0) {
+    alert("Você não receberá hora extra.")
+}
+
+else {
+    alert("")
+}
+
 
 
 // Exercício 12 (pendente)
